37ad321f5b556e76b1be4683ea0bae692fe8ab82
SallyMulupi <mulupisally2gmail.com>
2022-07-15 23:19:41 +0300

    add input onchange

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,6 +2,12 @@
 import React from 'react'
 const Keypad = () => {
     function fireEvent() {
-
+        console.log("Entering password...")
     }
+    return ( <
+        input onChange = { fireEvent }
+        type = "password" / >
+    )
 }
+
+export default Keypad;
